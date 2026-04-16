f80e1440a777ef7e9e584641d80fe3906833b946
Lucasvr-Dev <lucasvrr.dev@gmail.com>
2026-04-16 10:08:16 -0300

    atualizando server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +10,11 @@ app.use(express.json());
 
 const taskRoutes = require('./routes/taskRoutes');
 app.use('/api/tasks', taskRoutes);
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo conectado"))
+  .catch((err) => console.log("Erro Mongo:", err));
 
 
 app.get('/', (req, res) => {
